44815ea99e99e483b0f18be93afab3e7c816e632
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-03-07 00:04:01 +0200

    Mess around

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+/* 
+    Blackjack applicaiton
+    by Ilia Anastassov
+*/
+
+let cardOne = "Queen of Hearts",
+    cardTwo = "Jack of Spades";
+
+console.log("Welcome to Blackjack!");
+console.log("You have been dealt:");
+console.log("\t" + cardOne);
+console.log("\t" + cardTwo);
