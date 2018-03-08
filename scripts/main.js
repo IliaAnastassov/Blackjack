dd2898616519cae7d8f831d5e008e3f5a7ab410d
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-03-08 16:34:51 +0200

    Add deck of cards; add array for holding player cards; add randomNumber function

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,33 @@
     by Ilia Anastassov
 */
 
-let cardOne = "Queen of Hearts",
-    cardTwo = "Jack of Spades";
+function randomNumber(range) {
+    return Math.round(Math.random() * range);
+}
+
+let deck = [
+    "Two of Spades",
+    "Three of Spades",
+    "Four of Spades",
+    "Five of Spades",
+    "Six of Spades",
+    "Seven of Spades",
+    "Eight of Spades",
+    "Nine of Spades",
+    "Ten of Spades",
+    "Jack of Spades",
+    "Queen of Spades",
+    "King of Spades",
+    "Ace of Spades"
+];
+
+let playerCards = [
+    deck.splice(randomNumber(deck.length - 1), 1),
+    deck.splice(randomNumber(deck.length - 1), 1)
+];
 
 console.log("Welcome to Blackjack!");
+
 console.log("You have been dealt:");
-console.log("\t" + cardOne);
-console.log("\t" + cardTwo);
+console.log("\t" + playerCards[0]);
+console.log("\t" + playerCards[1]);
