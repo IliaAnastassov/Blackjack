8533201147221d4d7cab2c14af198033f3ce93ef
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-03-09 00:19:38 +0200

    Add all 52 cars to the deck using nested loops

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,20 @@
     by Ilia Anastassov
 */
 
+let suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+let ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+
+let deck = [];
+
+suits.forEach(suit => {
+    ranks.forEach(rank => {
+        deck.push(rank + " of " + suit);
+    });
+});
+
 function randomNumber(range) {
     return Math.round(Math.random() * range);
 }
-
-let deck = [
-    "Two of Spades",
-    "Three of Spades",
-    "Four of Spades",
-    "Five of Spades",
-    "Six of Spades",
-    "Seven of Spades",
-    "Eight of Spades",
-    "Nine of Spades",
-    "Ten of Spades",
-    "Jack of Spades",
-    "Queen of Spades",
-    "King of Spades",
-    "Ace of Spades"
-];
 
 let playerCards = [
     deck.splice(randomNumber(deck.length - 1), 1),
